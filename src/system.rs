f1245fb3090ffeb49244ad7b5dd905456ae649db
cr-eative-dev <github@mail.cr.eative.dev>
2025-01-25 13:59:25 +0100

    added error handling even though its unlikely there will be errors

--- src/system.rs
+++ src/system.rs
@@ -11,10 +11,41 @@ pub struct Pallet {
 	pub nonce: BTreeMap<String, u32>,
 }
 
+#[derive(Debug)]
+pub enum SystemError {
+	NonceOverflow,
+	BlockNumberOverflow,
+}
+
 impl Pallet {
 	/// Create a new instance of the System Pallet.
 	pub fn new() -> Self {
 		/* TODO: Return a new instance of the `Pallet` struct. */
 		Pallet { block_number: 0, nonce: BTreeMap::new() }
+	}
+
+	/// Get the current block number.
+	pub fn block_number(&self) -> u32 {
+		/* TODO: Return the current block number. */
+		self.block_number
+	}
+
+	/// Increment the block number by one.
+	/// Returns an error if the block number would overflow.
+	pub fn inc_block_number(&mut self) -> Result<(), SystemError> {
+		/* TODO: Increment the current block number by one. */
+		self.block_number =
+			self.block_number.checked_add(1).ok_or(SystemError::BlockNumberOverflow)?;
+		Ok(())
+	}
+
+	/// Increment the nonce of an account.
+	/// Returns an error if the nonce would overflow.
+	pub fn inc_nonce(&mut self, who: &String) -> Result<(), SystemError> {
+		/* TODO: Get the current nonce of `who`, and increment it by one. */
+		let current_nonce = self.nonce.get(who).copied().unwrap_or(0);
+		let new_nonce = current_nonce.checked_add(1).ok_or(SystemError::NonceOverflow)?;
+		self.nonce.insert(who.clone(), new_nonce);
+		Ok(())
 	}
 }
